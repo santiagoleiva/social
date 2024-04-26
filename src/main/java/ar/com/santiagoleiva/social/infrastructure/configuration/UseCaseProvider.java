6089ff46efe6a58e53b08281b65470dc4124b0ce
Santiago Leiva <santiagoeleiva@gmail.com>
2024-04-25 22:58:19 -0300

    feat(create_tweet): define use case

--- src/main/java/ar/com/santiagoleiva/social/infrastructure/configuration/UseCaseProvider.java
+++ src/main/java/ar/com/santiagoleiva/social/infrastructure/configuration/UseCaseProvider.java
@@ -1,22 +1,8 @@
 package ar.com.santiagoleiva.social.infrastructure.configuration;
 
-import ar.com.santiagoleiva.social.application.port.CreateTweetPort;
-import ar.com.santiagoleiva.social.application.port.FindUserPort;
-import ar.com.santiagoleiva.social.application.usecase.PublishTweetUseCase;
-import ar.com.santiagoleiva.social.infrastructure.configuration.BeanProvider.TweetsConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class UseCaseProvider {
-
-    @Bean
-    public PublishTweetUseCase publishTweetUseCase(
-            TweetsConfiguration tweetsConfiguration,
-            CreateTweetPort createTweetPort,
-            FindUserPort findUserPort
-    ) {
-        return new PublishTweetUseCase(tweetsConfiguration, createTweetPort, findUserPort);
-    }
 
 }
