e4890ce47aa8e415a2e32728026edecbe9c8a593
Santiago Leiva <santiagoeleiva@gmail.com>
2024-04-27 13:13:23 -0300

    feat(follow): WIP - mapping relations

--- src/main/java/ar/com/santiagoleiva/social/infrastructure/jdbc/model/UserJdbcModel.java
+++ src/main/java/ar/com/santiagoleiva/social/infrastructure/jdbc/model/UserJdbcModel.java
@@ -2,10 +2,18 @@ package ar.com.santiagoleiva.social.infrastructure.jdbc.model;
 
 import ar.com.santiagoleiva.social.domain.User;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.MappedCollection;
 import org.springframework.data.relational.core.mapping.Table;
 
+import java.util.Set;
+
 @Table("user")
-public record UserJdbcModel(@Id Long id, String username) {
+public record UserJdbcModel(
+        @Id Long id,
+        String username,
+        @MappedCollection(idColumn = "follower_user_id")
+        Set<UserJdbcModel> followers
+) {
 
     public User toDomain() {
         return new User(id, username);
