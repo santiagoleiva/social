9be45261834e74ae4867b2f41bb403ff7dc60c5c
Santiago Leiva <santiagoeleiva@gmail.com>
2024-04-27 23:58:59 -0300

    feat(timeline): add test cases

---
+++ src/test/java/ar/com/santiagoleiva/social/application/usecase/GetUserTimelineUseCaseTest.java
@@ -0,0 +1,26 @@
+package ar.com.santiagoleiva.social.application.usecase;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@DisplayName("Get user timeline use case tests")
+class GetUserTimelineUseCaseTest {
+
+    @Autowired
+    private GetUserTimelineUseCase getUserTimelineUseCase;
+
+    @Test
+    @DisplayName("When user is not following to anyone it should return an empty list")
+    void whenUserIsNotFollowingToAnyoneItShouldReturnAnEmptyList() {
+        assertThat(getUserTimelineUseCase.execute(1L))
+                .isEmpty();
+    }
+
+}
